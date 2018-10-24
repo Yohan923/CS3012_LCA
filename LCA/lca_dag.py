98e6bac627a842797cd7531041309c08fa358c2e
John Zhang <johnzhang@Johns-MacBook-Air.local>
2018-10-24 15:29:46 +0100

    implemented some lca for dags

--- LCA/lca_dag.py
+++ LCA/lca_dag.py
@@ -1,9 +1,34 @@
 import networkx as nx
 
+
 class LCA_Dag:
 
     def find_lca(self, graph, a, b):
-        return 0
+        if graph is not None:
+            if nx.is_directed_acyclic_graph(graph):
+                anc_a = nx.ancestors(graph, a)
+                anc_b = nx.ancestors(graph, b)
+
+                common_anc = anc_a.intersection(anc_b)
+                lcas = list()
+                min_path = 0
+                for i in common_anc:
+                    t = nx.shortest_path_length(graph, i, a) + nx.shortest_path_length(graph, i, b)
+                    if min_path != 0:
+                        if min_path > t:
+                            lcas.clear()
+                            min_path = t
+                            lcas.append(i)
+                        elif min_path == t:
+                            lcas.append(i)
+                    else:
+                        min_path = t
+
+
+            else:
+                return "graph is not dag"
+        else:
+            return "graph is empty"
 
 
 def main():
