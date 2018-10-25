a16af7c09d072535c1ace99586f2ab9acee388d8
John Zhang <johnz0923@gmail.com>
2018-10-25 19:47:45 +0100

    added some more comments

--- test/test_binary_tree.py
+++ test/test_binary_tree.py
@@ -5,6 +5,7 @@ from LCA.binary_tree import BinaryTree
 
 class TestBinaryTree(unittest.TestCase):
 
+    # initialise trees used for testing
     def setUp(self):
         self.empty_tree = BinaryTree()
 
@@ -23,17 +24,19 @@ class TestBinaryTree(unittest.TestCase):
         file.close()
 
     def test_get_root(self):
+        # test get_root from empty tree
         root = self.empty_tree.get_root()
         self.assertEqual(None, root)
-
+        # test get_root from degenerative tree
         root = self.degenerative_tree.get_root()
         self.assertEqual(1, root.value)
-
+        # test get_root from normal tree with 10 nodes
         root = self.ten_node_tree.get_root()
         self.assertEqual(5, root.value)
 
     # can be used to test add()
     def test_tree_to_str(self):
+        # test tree to str on 10 node tree
         self.assertEqual("-5\n" +
                          " |-1\n" +
                          " | |-None\n" +
@@ -56,7 +59,7 @@ class TestBinaryTree(unittest.TestCase):
                          "     |  -None\n" +
                          "      -None\n"
                          , self.ten_node_tree.tree_to_str())
-
+        # test tree to str on degenerative tree
         self.assertEqual("-1\n" +
                          " |-None\n" +
                          "  -2\n" +
@@ -71,16 +74,20 @@ class TestBinaryTree(unittest.TestCase):
                          "           |-None\n" +
                          "            -None\n"
                          , self.degenerative_tree.tree_to_str())
-
+        # test tree to str on empty tree
         self.assertEqual("tree is empty", self.empty_tree.tree_to_str())
 
     def test_find(self):
+        # test find on empty tree
         self.assertEqual(None, self.empty_tree.find(12))
-
+        # test find on 10 node tree
         self.assertEqual(12, self.ten_node_tree.find(12).value)
-
+        # test find on 10 node tree where node does not exist in tree
         self.assertEqual(None, self.ten_node_tree.find(2))
+        # test find on degenerative tree
+        self.assertEqual(2, self.degenerative_tree.find(2).value)
 
+    # clean up
     def tearDown(self):
         self.empty_tree = None
         self.ten_node_tree = None
--- test/test_lca.py
+++ test/test_lca.py
@@ -5,14 +5,17 @@ from LCA.binary_tree import BinaryTree
 
 class TestLCA(unittest.TestCase):
 
+    # initialise trees used for testing
     def setUp(self):
         self.empty_tree = BinaryTree()
+
         self.degenerative_tree = BinaryTree()
         file = open("../resources/one_child_node.txt", 'r')
         values = file.readlines()
         for x in values:
             self.degenerative_tree.add(int(x.strip()))
         file.close()
+
         self.ten_node_tree = BinaryTree()
         file = open("../resources/ten_node_tree.txt", 'r')
         values = file.readlines()
@@ -20,17 +23,20 @@ class TestLCA(unittest.TestCase):
             self.ten_node_tree.add(int(x.strip()))
         file.close()
 
+    # tests for lca
     def test_lca(self):
+        # testing lca in a normal binary tree where root is lca
         self.assertEqual(5, LCA().lca(self.ten_node_tree, 3, 12).value)
-
+        # testing lca in a normal binary tree where lca is in right subtree
         self.assertEqual(17, LCA().lca(self.ten_node_tree, 10, 85).value)
-
+        # testing lca in a normal binary tree where lca does not exist as one of the node is not present in the tree
         self.assertEqual(None, LCA().lca(self.ten_node_tree, 1, 2))
-
+        # testing lca in an empty tree
         self.assertEqual(None, LCA().lca(self.empty_tree, 1, 2))
-
+        # testing lca in a degenerative tree
         self.assertEqual(1, LCA().lca(self.degenerative_tree, 1, 2).value)
 
+    # clean up
     def tearDown(self):
         self.empty_tree = None
         self.ten_node_tree = None
