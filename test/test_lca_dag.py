0615cde9b0f746e6a9617f5e5ba1b8b6605a22b1
John Zhang <johnz0923@gmail.com>
2018-10-28 11:13:56 +0000

    added tests for a cyclic directed graph

--- test/test_lca_dag.py
+++ test/test_lca_dag.py
@@ -1,10 +1,12 @@
-import  unittest
+import unittest
 import networkx as nx
 from LCA.lca_dag import LCA_Dag
+
 
 class TestLcaDag(unittest.TestCase):
 
     def setUp(self):
+        # read data to create dag with ten nodes
         g = nx.DiGraph()
         edges = list()
         file = open("../resources/ten_node_dag.txt", 'r')
@@ -12,21 +14,35 @@ class TestLcaDag(unittest.TestCase):
             values = lines.split(" ")
             edges.append(tuple(values))
         file.close()
-
         g.add_weighted_edges_from(edges)
-
         self.ten_node_dag = g
+
+        # read data to create cyclic directed graphs
+        g = nx.DiGraph()
+        edges = list()
+        file = open("../resources/cyclic_directed_graph.txt", 'r')
+        for lines in file:
+            values = lines.split(" ")
+            edges.append(tuple(values))
+        file.close()
+        g.add_weighted_edges_from(edges)
+        self.cyclic_directed_graph = g
+
+        # create empty graph
         self.empty_dag = nx.DiGraph()
 
     def test_find_lca(self):
+        # test normal case with only one lca for inputs a, b
         self.assertEqual(["12"], LCA_Dag().find_lca(self.ten_node_dag, "14", "12"))
-
+        # test normal case with multiple lcas for inputs a, b
         result = LCA_Dag().find_lca(self.ten_node_dag, "9", "10")
         result = set(result)
         expected = {"6", "8"}
         self.assertEqual(set(), expected.difference(result))
-
+        # test cases when one or two nodes are not found in the graph
         self.assertEqual("The node 0 is not in the graph.", str(LCA_Dag().find_lca(self.ten_node_dag, "0", "12")))
         self.assertEqual("The node 0 is not in the graph.", str(LCA_Dag().find_lca(self.ten_node_dag, "12", "0")))
-
+        # test case where the graph is not a dag ie. directed graph with cycles
+        self.assertEqual("graph is not dag", LCA_Dag().find_lca(self.cyclic_directed_graph, "1", "2"))
+        # test empty graph
         self.assertEqual("graph is empty", LCA_Dag().find_lca(self.empty_dag, "1", "12"))
